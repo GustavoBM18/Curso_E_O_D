3496edef5b53f16b684f103810f4d213343f5953
Gustavo Barrera Martinez <230110063@itsoeh.edu.mx>
2024-12-10 21:07:18 -0600

    Stacks T3

--- learning/Stacks/stack_sandbox.py
+++ learning/Stacks/stack_sandbox.py
@@ -6,30 +6,32 @@ logger = logging.getLogger(__name__)
 class Stack:
     def __init__(self, limit=1000):
         self.top_item = None
+        self.size = 0
+        self.limit = limit
+    def has_space(self):
+        return self.size < self.limit
+    def is_empty(self):
+        return self.size == 0
     def peek(self):
-        if self.top_item is None:
-            raise AttributeError("Stack is empty. No top item to peek.")
-        return self.top_item.get_value()
-    
-class Stack:
-    def __init__(self):
-        self.top_item = None
-
-    def peek(self):
-        # Return the value of the top item if it exists
-        if self.top_item is None:
-            raise AttributeError("Stack is empty. No top item to peek.")
-        return self.top_item.get_value()
+        if not self.is_empty():
+            return self.top_item.get_value()
+        else:
+            logger.warning("La pila esta totalmente vacia!")  # Log a warning
+            return None
     def push(self, value):
-        item = Node(value)
-        item.set_next_node(self.top_item)
-        self.top_item = item
+        if self.has_space():
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            self.size += 1
+        else:
+            logger.warning("La pila esta llena ¡No queda espacio!")  # Log a warning
     def pop(self):
-        if self.top_item is None: # check if it is none
-            raise AttributeError("Stack is empty. No item to pop.")
-        # save
-        item_to_remove = self.top_item
-        # update the top_item to the next node
-        self.top_item = item_to_remove.get_next_node()
-        # return value of removed item
-        return item_to_remove.get_value()
+        if not self.is_empty():
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            logger.warning("La pila esta totalmente vacia!")  # Log a warning
+            return None
