6cd12fcd9a6dc2eb85f2005330e8709e8ecee4e2
Gustavo Barrera Martinez <230110063@itsoeh.edu.mx>
2024-12-10 21:01:23 -0600

    Stacks T2

--- learning/Stacks/stack_sandbox.py
+++ learning/Stacks/stack_sandbox.py
@@ -10,3 +10,26 @@ class Stack:
         if self.top_item is None:
             raise AttributeError("Stack is empty. No top item to peek.")
         return self.top_item.get_value()
+    
+class Stack:
+    def __init__(self):
+        self.top_item = None
+
+    def peek(self):
+        # Return the value of the top item if it exists
+        if self.top_item is None:
+            raise AttributeError("Stack is empty. No top item to peek.")
+        return self.top_item.get_value()
+    def push(self, value):
+        item = Node(value)
+        item.set_next_node(self.top_item)
+        self.top_item = item
+    def pop(self):
+        if self.top_item is None: # check if it is none
+            raise AttributeError("Stack is empty. No item to pop.")
+        # save
+        item_to_remove = self.top_item
+        # update the top_item to the next node
+        self.top_item = item_to_remove.get_next_node()
+        # return value of removed item
+        return item_to_remove.get_value()
